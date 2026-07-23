240c05c09b67683e07b6f626e1231f106b99c16c
Wangcy1928 <3167519959@qq.com>
2026-07-23 13:43:39 +0800

    首页重定向到 /welcome.html

--- themes/icarus/layout/index.jsx
+++ themes/icarus/layout/index.jsx
@@ -1,22 +1,15 @@
-const { Component, Fragment } = require('inferno');
-const Paginator = require('hexo-component-inferno/lib/view/misc/paginator');
-const Article = require('./common/article');
+const { Component } = require('inferno');
 
 module.exports = class extends Component {
     render() {
-        const { config, page, helper } = this.props;
-        const { __, url_for } = helper;
-
-        return <Fragment>
-            {page.posts.map(post => <Article config={config} page={post} helper={helper} index={true} />)}
-            {page.total > 1 ? <Paginator
-                current={page.current}
-                total={page.total}
-                baseUrl={page.base}
-                path={config.pagination_dir}
-                urlFor={url_for}
-                prevTitle={__('common.prev')}
-                nextTitle={__('common.next')} /> : null}
-        </Fragment>;
+        // 直接重定向到欢迎页
+        return (
+            <html>
+                <head>
+                    <meta http-equiv="refresh" content="0; url=/welcome.html" />
+                </head>
+                <body></body>
+            </html>
+        );
     }
 };
